9f9431db6df74aca1b706fe1563ee64a2d219b21
Kalamon9 <kmonnier@hotmail.com>
2022-11-07 22:41:54 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,52 @@
+// alert("k");
+$(document).ready(function () {
+  $(".thing").slick({
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    fade: true,
+    cssEase: "linear",
+  });
+  $(".scroll-slick").slick({
+    infinite: false,
+    dots: true,
+    arrows: false,
+    speed: 300,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    centerMode: true,
+    centerPadding: "60px",
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 5,
+          slidesToScroll: 1,
+          centerMode: true,
+          centerPadding: "60px",
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          centerMode: true,
+          centerPadding: "40px",
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          centerMode: true,
+          centerPadding: "40px",
+        },
+      },
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ],
+  });
+});
